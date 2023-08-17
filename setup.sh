0a9a9767570777f41744e01a87fe6f5c3d9b7017
schultztimothy <schultz.timothy52@gmail.com>
2023-08-17 09:02:18 -0600

    remove usr cmd

--- setup.sh
+++ setup.sh
@@ -4,23 +4,23 @@ set -eu
 
 REDASH_BASE_PATH=/opt/redash
 
-install_docker(){
-    # Install Docker
-    export DEBIAN_FRONTEND=noninteractive
-    sudo apt-get -qqy update
-    DEBIAN_FRONTEND=noninteractive sudo -E apt-get -qqy -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade 
-    sudo apt-get -yy install apt-transport-https ca-certificates curl software-properties-common pwgen
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    sudo apt-get update && sudo apt-get -y install docker-ce
+# install_docker(){
+#     # Install Docker
+#     export DEBIAN_FRONTEND=noninteractive
+#     sudo apt-get -qqy update
+#     DEBIAN_FRONTEND=noninteractive sudo -E apt-get -qqy -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade 
+#     sudo apt-get -yy install apt-transport-https ca-certificates curl software-properties-common pwgen
+#     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#     sudo apt-get update && sudo apt-get -y install docker-ce
 
-    # Install Docker Compose
-    sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
+#     # Install Docker Compose
+#     sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+#     sudo chmod +x /usr/local/bin/docker-compose
 
-    # Allow current user to run Docker commands
-    sudo usermod -aG docker $USER
-}
+#     # Allow current user to run Docker commands
+#     sudo usermod -aG docker $USER
+# }
 
 create_directories() {
     if [[ ! -e $REDASH_BASE_PATH ]]; then
@@ -66,7 +66,7 @@ setup_compose() {
     sudo docker-compose up -d
 }
 
-install_docker
+# install_docker
 create_directories
 create_config
 setup_compose
